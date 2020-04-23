7f326cb75f9021fce79531276ba9017a9df2cb2d
oguz-ytp <63318939+oguz-ytp@users.noreply.github.com>
2020-04-24 01:30:18 +0300

    Delete dakikayi_saate_cevirme.py

--- dakikayi_saate_cevirme.py
+++
@@ -1,7 +0,0 @@
-girilen_dakika = input('Dakika girin : ')
-girilen_dakika = int(girilen_dakika)
-
-saat = (girilen_dakika // 60)
-dakika = girilen_dakika % 60
-
-print(girilen_dakika, 'dakika', saat, 'saat', dakika, 'dakikaya eşittir.')
