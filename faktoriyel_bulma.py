01ae492b092bdb7ed4a6e414bafe87ea2deaf141
oguz-ytp <63318939+oguz-ytp@users.noreply.github.com>
2020-04-24 00:30:22 +0300

    Delete faktoriyel_bulma.py

--- faktoriyel_bulma.py
+++
@@ -1,8 +0,0 @@
-sayi = input('Faktöriyelini bulmak istediğiniz sayıyı girin : ')
-sayi = int(sayi)
-
-sonuc = 1
-
-for i in range(1, sayi + 1):
-    sonuc = sonuc * i
-print(sayi, 'Sayısının faktöriyeli =', sonuc)
